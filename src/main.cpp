f48c4402a521f321694dd028b82d798c1e460e1c
Christian Helle <christian.helle@outlook.com>
2025-03-12 22:40:03 +0100

    Add functionality to open database from command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QStringList args = a.arguments();
+    if (args.length() > 1)
+    {
+        w.openDatabase(args.takeAt(1));
+    }
+
     w.show();
     return a.exec();
 }
