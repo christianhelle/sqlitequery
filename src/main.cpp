5dca4232ee64fbed895ba85486a6acf569da1c3c
Christian Helle <christian.helle@outlook.com>
2025-03-12 08:59:11 +0100

    Resolve undefined reference to __imp___argc on Windows build

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "mainwindow.h"
 
+#ifdef _WIN32
+#include <windows.h>
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    int argc = __argc;
+    char** argv = __argv;
+    
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+#else
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
     return a.exec();
 }
+#endif
