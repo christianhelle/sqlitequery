7d18458f3eff4d2cab61fc89ca931e3c4be1eccb
Christian Helle <christian.helle@outlook.com>
2025-03-12 20:44:49 +0100

    Update build script to correct windeployqt path and comment out release directory removal

--- src/build.ps1
+++ src/build.ps1
@@ -1,8 +1,8 @@
-Remove-Item .\release -Recurse -Force
+#Remove-Item .\release -Recurse -Force
 #qmake SQLiteAnalyzer.pro -config release
 cmake . -DCMAKE_PREFIX_PATH=C:/Qt/6.8.2/msvc2022_64 -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-"
 cmake --build . --config Release
 # mingw32-make -j32
 # mkdir .\release\bin
 # Move-Item .\release\SQLiteQueryAnalyzer.exe .\release\bin
-windeployqt .\release\SQLiteQueryAnalyzer.exe
+C:\Qt\6.8.2\msvc2022_64\bin\windeployqt.exe .\Release\SQLiteQueryAnalyzer.exe
