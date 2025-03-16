71e609a0627f04d8ca4a71c4b755484709e07211
Christian Helle <christian.helle@outlook.com>
2025-03-16 10:57:39 +0100

    Refactor build script to streamline the build process and remove commented-out code

--- src/build.ps1
+++ src/build.ps1
@@ -1,9 +1,4 @@
-#Remove-Item .\release -Recurse -Force
-#qmake SQLiteAnalyzer.pro -config release
 cmake . -DCMAKE_PREFIX_PATH=C:/Qt/6.8.2/msvc2022_64 -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" -B build
 cmake --build build --config Release
-# mingw32-make -j32
-# mkdir .\release\bin
-# Move-Item .\release\SQLiteQueryAnalyzer.exe .\release\bin
 C:\Qt\6.8.2\msvc2022_64\bin\windeployqt.exe .\build\Release\SQLiteQueryAnalyzer.exe
 ../deps/innosetup/ISCC.exe setup.iss
