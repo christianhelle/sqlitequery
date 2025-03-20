ce2c7d2dd09abc643bf22c5471a4ee9724e3059a
Christian Helle <christian.helle@outlook.com>
2025-03-20 15:29:35 +0100

    Fix quotes in values

--- src/dbexport.cpp
+++ src/dbexport.cpp
@@ -54,8 +54,8 @@ QStringList DbExport::getColumnValueDefinitions(const Table &table, const QSqlQu
         const auto value = query.value(column.name).toString();
         values[value] = column.dataType;
     }
-    auto keys = values.keys();
-    for (const auto &value: std::as_const(keys)) {
+    const auto keys = values.keys();
+    for (auto value: keys) {
         bool isText = false;
         for (const auto &type: textTypes) {
             if (values[value].contains(type, Qt::CaseInsensitive)) {
@@ -64,7 +64,8 @@ QStringList DbExport::getColumnValueDefinitions(const Table &table, const QSqlQu
             }
         }
         if (isText) {
-            valueDefinitions.append(QString("\"%1\"").arg(value));
+            const auto val = value.replace("\"", "\"\"");
+            valueDefinitions.append(QString("\"%1\"").arg(val));
         } else {
             valueDefinitions.append(value);
         }
