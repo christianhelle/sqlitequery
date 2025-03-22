3ced55d52eef6752d923b5fe36e26da57d2c2e6a
Christian Helle <christian.helle@outlook.com>
2025-03-22 22:57:01 +0100

    Add main window state to settings

--- src/settings.h
+++ src/settings.h
@@ -1,13 +1,21 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <QString>
+#include <QPoint>
+#include <QSizeF>
+
+struct WindowState {
+    QSize size;
+    QPoint position;
+};
 
 class Settings {
 public:
     static void init();
 
     static QString getSettingsFolder();
+    static void getMainWindowState(WindowState *state);
+    static void setMainWindowState(const QSizeF &size, const QPoint &position);
 };
 
 
