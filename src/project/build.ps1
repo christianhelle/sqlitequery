4a0fa1053133bc87a9fe66212f8612577566fd6e
Christian Helle <christian.helle@outlook.com>
2025-04-02 14:29:56 +0200

    Generate MD5 and SHA1 checksums from build.ps1

--- src/project/build.ps1
+++ src/project/build.ps1
@@ -4,12 +4,8 @@ param (
 )
 
 if ($IsWindows) {
-    cmake . -DCMAKE_PREFIX_PATH=C:/Qt/6.9.0/msvc2022_64 -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" -B build
+    cmake . -DCMAKE_PREFIX_PATH=C:/Qt/6.8.2/msvc2022_64 -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" -B build
     cmake --build build --config Release --parallel 32
-    if ($Package) {
-        C:\Qt\6.9.0\msvc2022_64\bin\windeployqt.exe .\build\Release\SQLiteQueryAnalyzer.exe
-        ../../deps/innosetup/ISCC.exe setup.iss
-    }
 } 
 
 if ($IsLinux) {
@@ -34,6 +30,18 @@ if ($IsLinux) {
             Write-Warning "To install snapcraft, run: sudo snap install snapcraft --classic"
         }
     }
+}
+
+if ($IsWindows -And $package) {
+    C:\Qt\6.8.2\msvc2022_64\bin\windeployqt.exe .\build\Release\SQLiteQueryAnalyzer.exe
+    
+    (Get-FileHash build/Release/SQLiteQueryAnalyzer.exe -Algorithm SHA1 | Select-Object).Hash 
+    | Set-Content build/Release/SQLiteQueryAnalyzer.exe.sha1
+
+    (Get-FileHash build/Release/SQLiteQueryAnalyzer.exe -Algorithm MD5 | Select-Object).Hash 
+    | Set-Content build/Release/SQLiteQueryAnalyzer.exe.md5
+
+    ../../deps/innosetup/ISCC.exe setup.iss
 }
 
 if ($IsMacOS -And $Package) {
