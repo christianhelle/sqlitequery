5173d5f3181c0d3b2045c85d82360f903a18884f
Christian Helle <christian.helle@outlook.com>
2025-04-02 15:10:01 +0200

    Remove whitespaces from build.ps1

--- src/project/build.ps1
+++ src/project/build.ps1
@@ -6,7 +6,7 @@ param (
 if ($IsWindows) {
     cmake . -DCMAKE_PREFIX_PATH=C:/Qt/6.8.2/msvc2022_64 -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" -B build
     cmake --build build --config Release --parallel 32
-} 
+}
 
 if ($IsLinux -or $IsMacOS) {
     cmake -B build
@@ -15,7 +15,7 @@ if ($IsLinux -or $IsMacOS) {
 
 if ($IsWindows -And $package) {
     C:\Qt\6.8.2\msvc2022_64\bin\windeployqt.exe .\build\Release\SQLiteQueryAnalyzer.exe
-    
+
     (Get-FileHash build/Release/SQLiteQueryAnalyzer.exe -Algorithm SHA1 | Select-Object).Hash 
     | Set-Content build/Release/SQLiteQueryAnalyzer.exe.sha1
 
