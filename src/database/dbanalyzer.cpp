36496cd60132858dc728b12e7e5ae9358e682faa
Christian Helle <christian.helle@outlook.com>
2025-04-08 23:18:39 +0200

    Skip sqlite_stat1 table in query results processing

--- src/database/dbanalyzer.cpp
+++ src/database/dbanalyzer.cpp
@@ -35,7 +35,7 @@ void DbAnalyzer::loadTables(DatabaseInfo &info) const {
     while (query.next()) {
         Table table;
         table.name = query.value("name").toString();
-        if (table.name == "sqlite_sequence") {
+        if (table.name == "sqlite_sequence" || table.name == "sqlite_stat1") {
             continue;
         }
         info.tables.append(table);
