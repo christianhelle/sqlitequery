118073eab01e3c94c41e7b552ecf0c5701e96c0e
Christian Helle <christian.helle@outlook.com>
2025-04-09 17:04:44 +0200

    Install libglx-dev libgl1-mesa-dev

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
+sudo apt install -y libglx-dev libgl1-mesa-dev
 pip install aqtinstall --break-system-packages
 aqt install-qt linux desktop 6.8.2 --autodesktop
