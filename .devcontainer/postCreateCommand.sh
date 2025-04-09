075a1f5c02193c146407dd2ec72d706d2ded8c19
Christian Helle <christian.helle@outlook.com>
2025-04-09 16:57:58 +0200

    Use --autodesktop instead of -m all

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 pip install aqtinstall --break-system-packages
-aqt install-qt linux desktop 6.8.2 -m all
+aqt install-qt linux desktop 6.8.2 --autodesktop
