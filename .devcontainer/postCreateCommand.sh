f62c214b39a8769cc661da691e150fbf7531ea5e
Christian Helle <christian.helle@outlook.com>
2025-04-09 17:27:11 +0200

    Install libdbus-1-dev

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
-sudo apt install -y libglx-dev libgl1-mesa-dev libxkbcommon-x11-dev libfontconfig1
+sudo apt install -y libglx-dev libgl1-mesa-dev libxkbcommon-x11-dev libfontconfig1 libdbus-1-dev
 pip install aqtinstall --break-system-packages
 aqt install-qt linux desktop 6.8.2 --autodesktop
