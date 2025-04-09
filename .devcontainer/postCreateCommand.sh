0aca3ffd5d1dfaece518074160b8f849061b76d5
Christian Helle <christian.helle@outlook.com>
2025-04-09 19:41:29 +0000

    export QT_DIR and Qt6_DIR env variables

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,6 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 sudo apt install -y libglx-dev libgl1-mesa-dev libxkbcommon-x11-dev libfontconfig1 libdbus-1-dev
 pip install aqtinstall --break-system-packages
-aqt install-qt linux desktop 6.8.2 --autodesktop
+aqt install-qt linux desktop 6.8.2
+export QT_DIR=./6.8.2/gcc_64
+export Qt6_DIR=./6.8.2/gcc_64
