a1095a6a19027e514d3e383b9152aae39b2f5254
Christian Helle <christian.helle@outlook.com>
2025-04-13 22:23:50 +0200

    Add printf progress tracking to ExportDataProgress class

--- src/database/progress.h
+++ src/database/progress.h
@@ -4,6 +4,7 @@
 class ExportDataProgress {
     uint64_t affectedRows = 0;
     bool isComplete = false;
+    bool printProgress = false;
 
 public:
     void reset() {
@@ -14,11 +15,16 @@ public:
     void increment() {
         affectedRows++;
         isComplete = false;
+        if (printProgress) {
+            printf("Progress: %llu rows processed.\n", affectedRows);
+        }
     }
 
     [[nodiscard]] uint64_t getAffectedRows() const { return affectedRows; }
     [[nodiscard]] bool isCompleted() const { return isComplete; }
     void setCompleted() { isComplete = true; }
+    void setShowProgress(bool value) { printProgress = value; }
+    [[nodiscard]] bool isShowProgress() const { return printProgress; }
 };
 
 #endif //PROGRESS_H
