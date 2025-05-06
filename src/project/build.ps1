165d4428f2b98cf5dbe0e308fdd411958c8a3465
Christian Helle <christian.helle@outlook.com>
2025-05-06 16:30:57 +0200

    Rename $package to $Package

--- src/project/build.ps1
+++ src/project/build.ps1
@@ -9,7 +9,7 @@ param (
 if ($IsWindows) {
     cmake . -DCMAKE_PREFIX_PATH=C:/Qt/6.9.0/msvc2022_64 -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" -B build
     cmake --build build --config Release --parallel 32
-    if ($package) {
+    if ($Package) {
         C:\Qt\6.9.0\msvc2022_64\bin\windeployqt.exe .\build\Release\SQLiteQueryAnalyzer.exe
         ../../deps/innosetup/ISCC.exe setup.iss
     }
@@ -32,11 +32,11 @@ if ($IsLinux) {
     }
 }
 
-if ($IsMacOS -And $package) {
+if ($IsMacOS -And $Package) {
     
     cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/tmp/sqlitequery
     cmake --build build --config Release --parallel 32
-    if ($package) {
+    if ($Package) {
         macdeployqt build/SQLiteQueryAnalyzer.app -dmg -appstore-compliant
     }
 }
